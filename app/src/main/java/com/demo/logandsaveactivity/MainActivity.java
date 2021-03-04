4c8b422a159fdf1d9f11c16a6cde1a75032d30f3
avasilistov <kvoroncova@gmail.com>
2021-03-04 13:03:19 +0200

    Added pick image from gallery

--- app/src/main/java/com/demo/logandsaveactivity/MainActivity.java
+++ app/src/main/java/com/demo/logandsaveactivity/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.FileProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
@@ -60,53 +61,31 @@ public class MainActivity extends AppCompatActivity {
         toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
         imgContainer = findViewById(R.id.image_view_container);
 
-//       Glide.with(this).load("http://goo.gl/gEgYUd").fitCenter().into(imgContainer);
 
-//        Log.i("zzz", MediaStore.Images.Media.getContentUri("images"));
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onAddButtonClicked();
-            }
+        addBtn.setOnClickListener(v -> onAddButtonClicked());
+
+        shareBtn.setOnClickListener(v -> {
+            File filePath = new File(getBaseContext().getFilesDir(), "");
+            File newFile = new File(filePath, "Log.txt");
+            Uri contentUri = getUriForFile(getBaseContext(), "com.demo.logandsaveactivity", newFile);
+            Intent shareFileIntent = new Intent();
+            shareFileIntent.setAction(Intent.ACTION_SEND);
+            shareFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            shareFileIntent.setType("text/plain");
+            shareFileIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+            startActivity(Intent.createChooser(shareFileIntent, "Share"));
         });
 
-        shareBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        cameraBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent();
-            }
-        });
+        cameraBtn.setOnClickListener(v -> dispatchTakePictureIntent());
 
         imageBtn.setOnClickListener(v -> {
-            Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(pickPhoto, 1);
+//            Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            startActivityForResult(pickPhoto, 1);
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, 1);
                 });
 
-// Share log file
-
-//        shareButton = findViewById(R.id.share_button);
-//        shareButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                File filePath = new File(getBaseContext().getFilesDir(), "");
-//                File newFile = new File(filePath, "Log.txt");
-//                Uri contentUri = getUriForFile(getBaseContext(), "com.demo.logandsaveactivity", newFile);
-//                Intent shareFileIntent = new Intent();
-//                shareFileIntent.setAction(Intent.ACTION_SEND);
-//                shareFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                shareFileIntent.setType("text/plain");
-//                shareFileIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
-//                startActivity(Intent.createChooser(shareFileIntent, "Share"));
-//            }
-//        });
 
 
         writeCall("onCreate", this);
@@ -120,21 +99,24 @@ public class MainActivity extends AppCompatActivity {
 
         if (resultCode == RESULT_OK && data != null){
             Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-            if (selectedImage != null){
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                Log.i("zzz", "null");
-                if (cursor!= null){
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    String picturePath = cursor.getString(columnIndex);
-                    Log.i("zzz", picturePath);
-//                    imgContainer.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-                    Glide.with(this).load(picturePath).fitCenter().into(imgContainer);
-                    cursor.close();
-                }
-            }
+            Glide.with(this).load(selectedImage).fitCenter().into(imgContainer);
+
+//            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+//            if (selectedImage != null){
+//                Cursor cursor = getContentResolver().query(selectedImage,
+//                        filePathColumn, null, null, null);
+//                Log.i("zzz", "null");
+//                if (cursor!= null){
+//                    cursor.moveToFirst();
+//                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//                    String picturePath = cursor.getString(columnIndex);
+//                    Log.i("zzz", picturePath);
+//                    Glide.with(this).load(picturePath).fitCenter().into(imgContainer);
+//                    cursor.close();
+//                }
+//            }
+//        }
+
         }
 
     }
