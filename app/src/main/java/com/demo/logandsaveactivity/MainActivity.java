997015a7609f4de66b3ea51fc97c11b089b5ebcc
avasilistov <kvoroncova@gmail.com>
2021-03-06 18:27:01 +0200

    Extracted methods for file handle to separate class. Todo requestpermission

--- app/src/main/java/com/demo/logandsaveactivity/MainActivity.java
+++ app/src/main/java/com/demo/logandsaveactivity/MainActivity.java
@@ -35,6 +35,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static androidx.core.content.FileProvider.getUriForFile;
+import static com.demo.logandsaveactivity.Utils.dispatchTakePictureIntent;
+import static com.demo.logandsaveactivity.Utils.getAndShow;
 import static com.demo.logandsaveactivity.Utils.writeCall;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     private Animation fromBottom;
     private Animation toBottom;
     private ImageView imgContainer;
-    private String currentPhotoPath;
+    private String [] currentPhotoPath = {""};
     private ConstraintLayout mLayout;
 
 
@@ -61,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     // There are no request codes
                     Intent data = result.getData();
-                    getAndShow(data);
+                    getAndShow(data, this, imgContainer);
                 }
             });
 
@@ -71,42 +73,10 @@ public class MainActivity extends AppCompatActivity {
             result -> {
                 if (result.getResultCode() == Activity.RESULT_OK) {
                     // There are no request codes
-                    Glide.with(this).load(currentPhotoPath).fitCenter().into(imgContainer);
+                    Glide.with(this).load(currentPhotoPath[0]).fitCenter().into(imgContainer);
                 }
             });
 
-
-
-    private void getAndShow(@Nullable Intent data) {
-        String picturePath = "";
-        if (data != null) {
-            Uri selectedImage = data.getData();
-            Log.i("zzz", "Uri selected image");
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-            if (selectedImage != null) {
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                Log.i("zzz", "cursor");
-                if (cursor != null) {
-                    Log.i("zzz", "cursor not null");
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    Log.i("zzz", "" + columnIndex);
-                     picturePath = cursor.getString(columnIndex);
-                    Log.i("zzz", picturePath);
-                    Glide.with(this).load(picturePath).fitCenter().into(imgContainer);
-                    cursor.close();
-                }
-            }
-        }
-
-    }
-
-    private void accessDenyed() {
-        Snackbar.make(mLayout, "Access denyed", Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.ok, view -> {
-                }).show();
-    }
 
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -131,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         shareBtn.setOnClickListener(v -> {
             File filePath = new File(getBaseContext().getFilesDir(), "");
             File newFile = new File(filePath, "Log.txt");
-            Uri contentUri = getUriForFile(getBaseContext(), "com.demo.logandsaveactivity", newFile);
+            Uri contentUri = getUriForFile(getBaseContext(), "com.demo.logandsaveactivity.fileprovider", newFile);
             Intent shareFileIntent = new Intent();
             shareFileIntent.setAction(Intent.ACTION_SEND);
             shareFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -140,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(Intent.createChooser(shareFileIntent, "Share"));
         });
 
-        cameraBtn.setOnClickListener(v -> dispatchTakePictureIntent());
+        cameraBtn.setOnClickListener(v -> dispatchTakePictureIntent(this, photoActivityResultLauncher, currentPhotoPath));
 
         imageBtn.setOnClickListener(v -> {
             Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -153,15 +123,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    private String getFileName(Uri selectedImage, Context context) {
-//        Cursor returnCursor =
-//                context.getContentResolver().query(selectedImage, null, null, null, null);
-//        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-//        returnCursor.moveToFirst();
-//        String fileName = returnCursor.getString(nameIndex);
-//        returnCursor.close();
-//        return fileName;
-//    }
+
 
 
     private void onAddButtonClicked() {
@@ -202,42 +164,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private File createImageFile() throws IOException {
-        //Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss")
-                .format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-        currentPhotoPath = image.getAbsolutePath();
-        Log.i("zzz", currentPhotoPath);
-        return image;
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-                Log.i("zzz", "file created");
-            } catch (IOException ex) {
-
-            }
-
-            if (photoFile !=null){
-               Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.demo.logandsaveactivity.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                photoActivityResultLauncher.launch(takePictureIntent);
-            }
-        }
-    }
 
 
     @Override
--- app/src/main/java/com/demo/logandsaveactivity/Utils.java
+++ app/src/main/java/com/demo/logandsaveactivity/Utils.java
@@ -2,11 +2,19 @@ package com.demo.logandsaveactivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
+
+import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -37,6 +45,68 @@ public class Utils {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+
+    public static File createImageFile(Context context, String [] paths) throws IOException {
+        //Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss")
+                .format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        paths[0] = image.getAbsolutePath();
+
+        return image;
+    }
+
+    public static void dispatchTakePictureIntent(Context context, ActivityResultLauncher launcher, String [] paths) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile(context, paths);
+                Log.i("zzz", "file created");
+            } catch (IOException ex) {
+
+            }
+
+            if (photoFile !=null){
+                Uri photoURI = FileProvider.getUriForFile(context,
+                        "com.demo.logandsaveactivity.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                launcher.launch(takePictureIntent);
+            }
+        }
+    }
+
+    public static void getAndShow(@Nullable Intent data, Context context, ImageView imgContainer) {
+        String picturePath = "";
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            Log.i("zzz", "Uri selected image");
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            if (selectedImage != null) {
+                Cursor cursor = context.getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                Log.i("zzz", "cursor");
+                if (cursor != null) {
+                    Log.i("zzz", "cursor not null");
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    Log.i("zzz", "" + columnIndex);
+                    picturePath = cursor.getString(columnIndex);
+                    Log.i("zzz", picturePath);
+                    Glide.with(context).load(picturePath).fitCenter().into(imgContainer);
+                    cursor.close();
+                }
+            }
         }
 
     }
